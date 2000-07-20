f0f470e0b5042dbae929c9cefe59d58bb734bcec
Shane Curcuru <curcuru@apache.org>
2000-07-20 20:05:15 +0000

    Begin cleaning up Xalan-J 2.x Ant makefile Attempt to separate compile targets; up -mx memory temporarily since we're compiling the whole tree at once...
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@333163 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -1,33 +1,37 @@
 @echo off
-
+rem Prerequisites: user must set JAVA_HOME
 if "%JAVA_HOME%" == "" goto error
-
 echo.
-echo Xalan-J Build
+echo Xalan-J 2.x Build
 echo -------------
 
+rem Default ANT_HOME if not set
+set SAVEANTHOME=%ANT_HOME%
+if "%ANT_HOME%" == "" set ANT_HOME=.
 
+rem This automatically adds system classes to CLASSPATH
+if exist %JAVA_HOME%\lib\tools.jar set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
+if exist %JAVA_HOME%\lib\classes.zip set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\classes.zip
+
+rem Default locations of jars we depend on 
 set ANT=bin\ant.jar
-set JAVAC=%JAVA_HOME%\lib\tools.jar
 set XERCES=bin\xerces.jar
 set BSF=bin\bsf.jar
 set BSFENGINES=bin\bsfengines.jar
 set DOCLET=xdocs\xalanjdoc.jar
 
-set HOLDINGPATH=%CLASSPATH%
-
 rem DOCLET must be on system CLASSPATH for javadocs task to work. Just including it in
 rem -classpath arg for java or javadoc call doesn't work....
-
-set CLASSPATH=%ANT%;%JAVAC%;%XERCES%;%BSF%;%BSFENGINES%;%DOCLET%;%CLASSPATH%
-
+set SAVECP=%CLASSPATH%
+set CLASSPATH=%ANT%;%XERCES%;%BSF%;%BSFENGINES%;%DOCLET%;%CLASSPATH%
+set CPSEP=;
 echo.
 echo Building with classpath %CLASSPATH%
-
-echo.
 echo Starting Ant...
 
-%JAVA_HOME%\bin\java.exe -Dant.home="%ANT_HOME%" -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+rem Temporarily, up the -mx memory since we compile a really big glob of files
+echo %JAVA_HOME%\bin\java.exe -mx64m -Dant.home="%ANT_HOME%" -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA_HOME%\bin\java.exe -mx64m -Dant.home="%ANT_HOME%" -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 goto end
 
@@ -39,12 +43,14 @@ echo "Please, set the JAVA_HOME environment variable to match the"
 echo "root directory of the Java Virtual Machine you want to use."
 
 :end
-
-set CLASSPATH=%HOLDINGPATH%
-set HOLDINGPATH=
+rem Cleanup environment variables
+set CLASSPATH=%SAVECP%
+set SAVECP=
+set CPSEP=
+set ANT_HOME=%SAVEANTHOME%
+set SAVEANTHOME=
 set XERCES=
 set BSF=
 set BSFENGINES=
 set DOCLET=
 set ANT=
-set JAVAC=
