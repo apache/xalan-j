353463bf27b784f5fd62b26f29392fbb56624c08
Scott Boag <sboag@apache.org>
2000-08-09 18:25:42 +0000

    Implement NamedNodeMap and getAttributes.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@333282 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/lib/sql/StreamableNode.java
+++ src/org/apache/xalan/lib/sql/StreamableNode.java
@@ -2,15 +2,18 @@ package org.apache.xalan.lib.sql;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.DOMException;
 import org.apache.xalan.utils.UnImplNode;
 import org.apache.xpath.patterns.NodeTestFilter;
 import org.apache.xpath.patterns.NodeTest;
+import org.apache.xalan.res.XSLTErrorResources;
 
 /**
  * This is the superclass for all nodes in the Xalan sql package.
  */
-public class StreamableNode extends UnImplNode implements NodeTestFilter
+public class StreamableNode extends UnImplNode 
+  implements NodeTestFilter, NamedNodeMap
 {
   private XStatement m_statement;
   
@@ -70,6 +73,59 @@ public class StreamableNode extends UnImplNode implements NodeTestFilter
   {
     return null;
   }
+  
+  public NamedNodeMap       getAttributes()
+  {
+    return this;
+  }
 
+  public Node getNamedItem(String name)
+  {
+    return null;
+  }
+    
+  public Node item(int index)
+  {
+    return null;
+  }
 
+  public int getLength()
+  {
+    return 0;
+  }
+
+  public Node getNamedItemNS(String namespaceURI, 
+                             String localName)
+  {
+    return null;
+  }
+  
+  public Node setNamedItem(Node arg)
+    throws DOMException
+  {
+    error(XSLTErrorResources.ER_FUNCTION_NOT_SUPPORTED); 
+    return null;
+  }
+  
+  public Node removeNamedItem(String name)
+    throws DOMException
+  {
+    error(XSLTErrorResources.ER_FUNCTION_NOT_SUPPORTED); 
+    return null;
+  }
+  
+  public Node setNamedItemNS(Node arg)
+    throws DOMException
+  {
+    error(XSLTErrorResources.ER_FUNCTION_NOT_SUPPORTED); 
+    return null;
+  }
+  
+  public Node removeNamedItemNS(String namespaceURI, 
+                                String localName)
+    throws DOMException
+  {
+    error(XSLTErrorResources.ER_FUNCTION_NOT_SUPPORTED); 
+    return null;
+  }
 }
