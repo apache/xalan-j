a2949c41aec2a11710782fecc23de55a85a86b79
Gary L Peskin <garyp@apache.org>
2000-10-17 17:06:42 +0000

    Implement nodeset extension function.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@333540 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xalan/lib/Extensions.java
@@ -0,0 +1,101 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, Lotus
+ * Development Corporation., http://www.lotus.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.xalan.lib;
+
+import org.w3c.dom.Text;
+import org.w3c.dom.traversal.NodeIterator;
+import org.apache.xpath.NodeSet;
+
+import org.apache.xalan.extensions.ExpressionContext;
+
+/**
+ * <meta name="usage" content="general"/>
+ * This class contains many of the Xalan-supplied extensions.
+ * It is accessed by specifying a namespace URI as follows:
+ * <pre>
+ *    xmlns:xalan="http://xml.apache.org/xalan"
+ * </pre>
+ */
+
+public class Extensions {
+
+  private Extensions() {}	// Make sure class cannot be instantiated
+
+  /**
+   * This method is an extension that implements as a Xalan extension
+   * the node-set function also found in xt and saxon.
+   * As described in section 11.1 of XSLT 1.0 Recommendation,
+   * the node-set returned contains just a single root node.  Most of the
+   * actual work here is done in <code>MethodResolver</code> and
+   * <code>XRTreeFrag</code>.
+   */
+
+  public static NodeSet nodeset(ExpressionContext myProcessor, Object rtf) {
+
+    if (rtf instanceof NodeIterator)
+    {
+      return new NodeSet((NodeIterator) rtf);
+    }
+    else
+    {
+      Text textNode = myProcessor.getContextNode().getOwnerDocument().createTextNode(rtf.toString());
+      return new NodeSet(textNode);
+    }
+  }
+
+}
