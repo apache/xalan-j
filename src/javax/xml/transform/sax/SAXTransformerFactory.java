4ef771bd9a6ff56b5135e7473de99dc4ff61e6bc
Scott Boag <sboag@apache.org>
2000-11-06 17:36:05 +0000

    Moved getAssociatedStylesheet up to generic level.  added newTransformerHandler(Templates templates).
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@333682 13f79535-47bb-0310-9956-ffa450edef68

--- src/javax/xml/transform/sax/SAXTransformerFactory.java
+++ src/javax/xml/transform/sax/SAXTransformerFactory.java
@@ -92,6 +92,18 @@ public abstract class SAXTransformerFactory extends TransformerFactory
 
   /**
    * Get a TransformerHandler object that can process SAX
+   * ContentHandler events into a Result, based on the Templates argument.
+   *
+   * @param templates The source of the transformation instructions.
+   *
+   * @return TransformerHandler ready to transform SAX events.
+   * @throws TransformerConfigurationException
+   */
+  public abstract TransformerHandler newTransformerHandler(Templates templates)
+    throws TransformerConfigurationException;
+
+  /**
+   * Get a TransformerHandler object that can process SAX
    * ContentHandler events into a Result.
    *
    * @param src The source of the transformation instructions.
@@ -125,30 +137,6 @@ public abstract class SAXTransformerFactory extends TransformerFactory
    * @return An XMLFilter object, or null if this feature is not supported.
    */
   public abstract XMLFilter newXMLFilter(Source src);
-
-  /**
-   * Get InputSource specification(s) that are associated with the
-   * given document specified in the source param,
-   * via the xml-stylesheet processing instruction
-   * (see http://www.w3.org/TR/xml-stylesheet/), and that matches
-   * the given criteria.  Note that it is possible to return several stylesheets
-   * that match the criteria, in which case they are applied as if they were
-   * a list of imports or cascades.
-   *
-   * @param source
-   * @param media The media attribute to be matched.  May be null, in which
-   *              case the prefered templates will be used (i.e. alternate = no).
-   * @param title The value of the title attribute to match.  May be null.
-   * @param charset The value of the charset attribute to match.  May be null.
-   * @returns An array of InputSources that can be passed to processMultiple method.
-   *
-   * @return A Source object suitable for passing to the TransformerFactory.
-   *
-   * @throws TransformerConfigurationException
-   */
-  public abstract Source getAssociatedStylesheet(
-    Source source, String media, String title, String charset)
-      throws TransformerConfigurationException;
   
   /** NEEDSDOC Field errorHandler          */
   ErrorHandler errorHandler;
