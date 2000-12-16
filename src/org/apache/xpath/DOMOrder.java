6a0f8dfcd683cb406c27597361b3034fd90f11a7
Joseph Jay Kesselman <jkesselm@apache.org>
2000-12-16 02:54:21 +0000

    Doc
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@334001 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xpath/DOMOrder.java
+++ src/org/apache/xpath/DOMOrder.java
@@ -58,7 +58,8 @@ package org.apache.xpath;
 
 /**
  * Nodes that implement this index can return a document order index.
- * Eventually, this will be replaced by DOM 3 methods.
+ * Eventually, this will be replaced by DOM 3 methods. 
+ * (compareDocumentOrder and/or compareTreePosition.)
  */
 public interface DOMOrder
 {
@@ -66,7 +67,10 @@ public interface DOMOrder
   /**
    * Get the UID (document order index).
    *
-   * NEEDSDOC ($objectName$) @return
+   * @return integer whose relative value corresponds to document order
+   * -- that is, if node1.getUid()<node2.getUid(), node1 comes before
+   * node2, and if they're equal node1 and node2 are the same node. No
+   * promises are made beyond that.
    */
   public int getUid();
 }
