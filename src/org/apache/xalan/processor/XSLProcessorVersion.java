63995a33c6d5d46228f502954b0f74c83e33a86e
Donald Leslie <dleslie@apache.org>
2001-01-23 22:02:55 +0000

    Version 2.0.0
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@334293 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/processor/XSLProcessorVersion.java
+++ src/org/apache/xalan/processor/XSLProcessorVersion.java
@@ -79,7 +79,7 @@ public class XSLProcessorVersion
    * Version String like <CODE>"<B>Xalan</B>i v.r[.dd| <B>D</B>nn]"</CODE>.
    * <P>Semantics of the version string are identical to the Xerces project.</P>
    */
-  public static String S_VERSION = "2.0_D07";
+  public static String S_VERSION = "2.0.0";
 
   /**
    * Constant name of product.
@@ -142,5 +142,5 @@ public class XSLProcessorVersion
    *          well as defect fixes. 'D' drops may not be as stable as
    *          the final releases.
    */
-  public static int DEVELOPMENT = 05;
+  public static int DEVELOPMENT = 0;
 }
