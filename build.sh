9ec033e13bfefb67f4614d9ab0f6c3abd5a59826
Shane Curcuru <curcuru@apache.org>
2001-04-23 21:20:29 +0000

    Update to use JAVA_OPTS to fix Bugzilla 1044; plus cleanup Suggestions for improvement to .sh script welcome!
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@334635 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -1,42 +1,38 @@
 #!/bin/sh
-#	Name:   build.sh
+#	Name:   build.sh Build Xalan-J 2.x using Ant
 #	Author: Shane Curcuru
 
-echo "build.sh beginning..."
+echo "Xalan-J 2.x Build"
+echo "-------------"
 
+_JAVACMD=$JAVA_HOME/bin/java
 if [ "$JAVA_HOME" = "" ] ; then
-    echo "You must set JAVA_HOME, sorry; also JDK 1.1x add classes.zip to CLASSPATH!"
-    exit 1
+    echo "Warning: JAVA_HOME environment variable is not set."
+    _JAVACMD=java
+fi
+
+# Default locations of jars we depend on to run Ant on our build.xml file
+if [ "$ANT_HOME" = "" ] ; then
+    ANT_HOME=.
+fi
+if [ "$ANT_JAR" = "" ] ; then
+    ANT_JAR=./bin/ant.jar
+fi
+if [ "$PARSER_JAR" = "" ] ; then
+    PARSER_JAR=./bin/xerces.jar
 fi
 
 # Default to UNIX-style pathing
 CLPATHSEP=:
 # if we're on a Windows box make it ;
 uname | grep WIN && CLPATHSEP=\;
-
-# Update SERVLET to point to JAR containing javax.servlet and javax.servlet.http
-#    packages; alternately include this in the classpath yourself
-SERVLET=/jswdk-1.0.1/lib/servlet.jar
-
-# Default locations for each of our jars; change as needed
-BINDIR=./bin
-ANT=$BINDIR/ant.jar
-TOOLS_JAR=$JAVA_HOME/lib/tools.jar
-XERCES=$BINDIR/xerces.jar
-BSF=$BINDIR/bsf.jar
-BSFENGINES=$BINDIR/bsfengines.jar
-DOCGENERATOR=$BINDIR/stylebook-1.0-b3_xalan-2.jar
-DOCLET=$BINDIR/xalanjdoc.jar
-
-# Make sure this classpath scheme works. I.e., can javadoc find xalanjdoc.Standard?
-TEMP_CP="$ANT${CLPATHSEP}$TOOLS_JAR${CLPATHSEP}$XERCES${CLPATHSEP}$BSF${CLPATHSEP}$BSFENGINES${CLPATHSEP}$DOCGENERATOR${CLPATHSEP}$DOCLET${CLPATHSEP}$CLASSPATH"
+_CLASSPATH="$ANT_JAR${CLPATHSEP}$PARSER_JAR${CLPATHSEP}$CLASSPATH"
 
 echo "Starting Ant with targets: $@"
-echo "        ...with classpath: $TEMP_CP"
+echo "        ...with classpath: $_CLASSPATH"
 
-"$JAVA_HOME"/bin/java -Dant.home=. -classpath "$TEMP_CP" org.apache.tools.ant.Main $@
+"$_JAVACMD" $JAVA_OPTS -Dant.home=$ANT_HOME -classpath "$_CLASSPATH" org.apache.tools.ant.Main $@
 
-echo "build.sh complete!"
 
 
 
