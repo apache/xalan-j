6c744cea897e139a72ade2943b0640e806bffd86
Costin Manolache <costin@apache.org>
2001-05-10 19:47:06 +0000

    Removed javax.xml.parsers from the generated jar file. The files are still in repository - to avoid build problems or new dependencies.
    Automatically add tools.jar in CLASSPATH ( as in build.bat )
    
    Added a note that build.sh can be replaced with using "ant" ( as people
    would use "make" ) - many other apache projects are using this "style".
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@334706 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 #	Name:   build.sh Build Xalan-J 2.x using Ant
 #	Author: Shane Curcuru
 
+# Alternatively, you can just call "ant" 
+
 echo "Xalan-J 2.x Build"
 echo "-------------"
 
@@ -22,11 +24,22 @@ if [ "$PARSER_JAR" = "" ] ; then
     PARSER_JAR=./bin/xerces.jar
 fi
 
+# Use _underscore prefix to not conflict with user's settings
 # Default to UNIX-style pathing
 CLPATHSEP=:
 # if we're on a Windows box make it ;
 uname | grep WIN && CLPATHSEP=\;
 _CLASSPATH="$ANT_JAR${CLPATHSEP}$PARSER_JAR${CLPATHSEP}$CLASSPATH"
+
+# Attempt to automatically add system classes to _CLASSPATH
+if [ -f $JAVA_HOME/lib/tools.jar ] ; then
+  _CLASSPATH=${_CLASSPATH}${CLPATHSEP}${JAVA_HOME}/lib/tools.jar
+fi
+
+if [ -f $JAVA_HOME/lib/classes.zip ] ; then
+  _CLASSPATH=${_CLASSPATH}${CLPATHSEP}${JAVA_HOME}/lib/classes.zip
+fi
+
 
 echo "Starting Ant with targets: $@"
 echo "        ...with classpath: $_CLASSPATH"
