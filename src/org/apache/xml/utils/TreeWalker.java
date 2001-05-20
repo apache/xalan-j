5370854ec39fe129676e30556ebe71b968e8593b
Shane Curcuru <curcuru@apache.org>
2001-05-20 00:56:00 +0000

    Catch SecurityException whenever accessing System.getProperty("user.dir"); minor javadoc update
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@334818 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/utils/TreeWalker.java
+++ src/org/apache/xml/utils/TreeWalker.java
@@ -109,7 +109,20 @@ public class TreeWalker
 		if (systemId != null)
 			m_locator.setSystemId(systemId);
 		else
-			m_locator.setSystemId(System.getProperty("user.dir"));
+		{
+            try
+            {
+    			m_locator.setSystemId(System.getProperty("user.dir"));
+            }
+            catch (SecurityException se)
+            {
+                // Must catch this for applet case -sc
+                // Um, set default to '.', for lack of a better idea?
+                // This may well not work, but it's probably better 
+                //  than just propagating the SecurityException
+    			m_locator.setSystemId(".");
+            }		    
+		}
     m_dh = dh;
   }
 
@@ -122,7 +135,19 @@ public class TreeWalker
   {
     this.m_contentHandler = contentHandler;
 		m_contentHandler.setDocumentLocator(m_locator);
-		m_locator.setSystemId(System.getProperty("user.dir"));
+        try
+        {
+    		m_locator.setSystemId(System.getProperty("user.dir"));
+        }
+        catch (SecurityException se)
+        {
+            // Must catch this for applet case -sc
+            // Um, set default to '.', for lack of a better idea?
+            // This may well not work, but it's probably better 
+            //  than just propagating the SecurityException
+    		m_locator.setSystemId(".");
+        }		    
+        
     m_dh = dh;
   }
   
@@ -135,7 +160,18 @@ public class TreeWalker
   {
     this.m_contentHandler = contentHandler;
 		m_contentHandler.setDocumentLocator(m_locator);
-		m_locator.setSystemId(System.getProperty("user.dir"));
+        try
+        {
+    		m_locator.setSystemId(System.getProperty("user.dir"));
+        }
+        catch (SecurityException se)
+        {
+            // Must catch this for applet case -sc
+            // Um, set default to '.', for lack of a better idea?
+            // This may well not work, but it's probably better 
+            //  than just propagating the SecurityException
+    		m_locator.setSystemId(".");
+        }		    
 		m_dh = new org.apache.xpath.DOM2Helper();
   }
 
