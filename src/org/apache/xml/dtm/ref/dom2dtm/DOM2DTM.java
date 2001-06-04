ae257bab5a7a1601880e2f4a161dbf0278b3eea9
Scott Boag <sboag@apache.org>
2001-06-04 13:49:36 +0000

    Added support for the normalize space arg on dispatchCharacters.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/branches/DTM_EXP@334908 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
+++ src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
@@ -1337,6 +1337,18 @@ public class DOM2DTM extends DTMDefaultBaseIterators
   }
 
   // ========== Direct SAX Dispatch, for optimization purposes ========
+  
+  /**
+   * Returns whether the specified <var>ch</var> conforms to the XML 1.0 definition
+   * of whitespace.  Refer to <A href="http://www.w3.org/TR/1998/REC-xml-19980210#NT-S">
+   * the definition of <CODE>S</CODE></A> for details.
+   * @param   ch      Character to check as XML whitespace.
+   * @return          =true if <var>ch</var> is XML whitespace; otherwise =false.
+   */
+  private static boolean isSpace(char ch)
+  {
+    return XMLCharacterRecognizer.isWhiteSpace(ch);  // Take the easy way out for now.
+  }
 
   /**
    * Directly call the
@@ -1356,9 +1368,18 @@ public class DOM2DTM extends DTMDefaultBaseIterators
           boolean normalize)
             throws org.xml.sax.SAXException
   {
-    int type = getNodeType(nodeHandle);
-    Node node = getNode(nodeHandle);
-    dispatchNodeData(node, ch, 0);
+    if(normalize)
+    {
+      XMLString str = getStringValue(nodeHandle);
+      str = str.fixWhiteSpace(true, true, false);
+      str.dispatchCharactersEvents(ch);
+    }
+    else
+    {
+      int type = getNodeType(nodeHandle);
+      Node node = getNode(nodeHandle);
+      dispatchNodeData(node, ch, 0);
+    }
   }
   
   /**
