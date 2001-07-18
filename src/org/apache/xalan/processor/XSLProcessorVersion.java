6695cef09dbb5fedacafe44672c05e681291f66f
Donald Leslie <dleslie@apache.org>
2001-07-18 14:00:34 +0000

    2.2.D7
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@335214 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/processor/XSLProcessorVersion.java
+++ src/org/apache/xalan/processor/XSLProcessorVersion.java
@@ -136,7 +136,7 @@ public class XSLProcessorVersion
    *          well as defect fixes. 'D' drops may not be as stable as
    *          the final releases.
    */
-  public static int DEVELOPMENT = 0;
+  public static int DEVELOPMENT = 7;
   
   /**
    * Version String like <CODE>"<B>Xalan</B> <B>Language</B> 
