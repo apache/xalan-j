1f7a0c692060bf06f1946acd09add918d26f71f2
Donald Leslie <dleslie@apache.org>
2001-08-29 15:13:22 +0000

    Updated version number to 2.2.D10
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@335449 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/processor/XSLProcessorVersion.java
+++ src/org/apache/xalan/processor/XSLProcessorVersion.java
@@ -136,7 +136,7 @@ public class XSLProcessorVersion
    *          well as defect fixes. 'D' drops may not be as stable as
    *          the final releases.
    */
-  public static int DEVELOPMENT = 9;
+  public static int DEVELOPMENT = 10;
   
   /**
    * Version String like <CODE>"<B>Xalan</B> <B>Language</B> 
