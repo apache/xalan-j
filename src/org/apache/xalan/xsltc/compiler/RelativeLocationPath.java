06642ab01878fc8398552d4e812380e168004f17
Morten Jorgensen <morten@apache.org>
2001-10-08 07:47:59 +0000

    Fix for ordering of certain node sets. PR:		bugzilla 3766 Obtained from:	n/a Submitted by:	morten@xml.apache.org Reviewed by:	morten@xml.apache.org
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@335602 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/xsltc/compiler/RelativeLocationPath.java
+++ src/org/apache/xalan/xsltc/compiler/RelativeLocationPath.java
@@ -64,6 +64,7 @@
 package org.apache.xalan.xsltc.compiler;
 
 abstract class RelativeLocationPath extends Expression {
+    public abstract int getAxis();
     public abstract void setAxis(int axis);
     public abstract boolean descendantAxis();
 }
