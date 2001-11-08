48eae5a9a00a3596c052dbf3bf7356d5e4dbfeac
Joseph Jay Kesselman <jkesselm@apache.org>
2001-11-08 19:14:53 +0000

    Bugzilla 4735: We should at least report an error when we overflow DTM addressing...
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@335803 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
+++ src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
@@ -79,6 +79,8 @@ import org.apache.xml.utils.NodeVector;
 
 import org.apache.xml.utils.XMLString;
 import org.apache.xml.utils.XMLStringFactory;
+import org.apache.xalan.res.XSLTErrorResources;
+import org.apache.xalan.res.XSLMessages;
 
 /** The <code>DOM2DTM</code> class serves up a DOM's contents via the
  * DTM API.
@@ -207,6 +209,15 @@ public class DOM2DTM extends DTMDefaultBaseIterators
                         int previousSibling, int forceNodeType)
   {
     int nodeIndex = m_nodes.size();
+    // Report DTM overflow
+    if(nodeIndex>DTMManager.IDENT_NODE_DEFAULT)
+    {
+      // %REVIEW% Wrong error message, but I've been told we're trying
+      // not to add messages right not for I18N reasons.
+      // %REVIEW% Should this be a Fatal Error?
+      error(XSLMessages.createMessage(XSLTErrorResources.ER_NO_DTMIDS_AVAIL, null));//"No more DTM IDs are available";
+    }
+
     m_size++;
     // ensureSize(nodeIndex);
     
