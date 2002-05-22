5d571640fb6f2b6a7bd428a41e5870b4f91955c4
Santiago Pericas-Geertsen <santiagopg@apache.org>
2002-05-22 22:43:25 +0000

    Experimental output system.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@336384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xalan/xsltc/runtime/output/TransletOutputHandlerFactory.java
@@ -0,0 +1,133 @@
+/*
+ * @(#)$Id$
+ *
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES{} LOSS OF
+ * USE, DATA, OR PROFITS{} OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Sun
+ * Microsystems., http://www.sun.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * @author Santiago Pericas-Geertsen
+ *
+ */
+
+package org.apache.xalan.xsltc.runtime.output;
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.ext.LexicalHandler;
+import org.apache.xalan.xsltc.runtime.*;
+import org.apache.xalan.xsltc.TransletOutputHandler;
+
+public class TransletOutputHandlerFactory {
+
+    public static final int STREAM = 0;
+    public static final int SAX    = 1;
+    public static final int DOM    = 2;
+
+    private String _encoding   = "utf-8";
+    private String _method     = "xml";
+    private int    _outputType = STREAM;
+
+    static public TransletOutputHandlerFactory newInstance() {
+	return new TransletOutputHandlerFactory();
+    }
+
+    public void setOutputType(int outputType) {
+	_outputType = outputType;
+    }
+
+    public void setEncoding(String encoding) {
+	if (encoding != null) {
+	    _encoding = encoding;
+	}
+    }
+
+    public void setOutputMethod(String method) {
+	if (method != null) {
+	    _method = method;
+	}
+    }
+
+    public TransletOutputHandler getTransletOutputHandler() throws IOException {
+	switch (_outputType) {
+	    case STREAM:
+		if (_method.equalsIgnoreCase("xml")) {
+		    return new StreamXMLOutput(System.out, _encoding);
+		}
+		else if (_method.equalsIgnoreCase("html")) {
+		    return new StreamHTMLOutput(System.out, _encoding);
+		}
+		else if (_method.equalsIgnoreCase("text")) {
+		    return null;		// TODO
+		}
+	    break;
+	    case SAX:
+		// TODO
+	    break;
+	    case DOM:
+		// TODO
+	    break;
+	}
+	return null;
+    }
+
+    // Temporary - returns an instance of TextOutput
+    public TransletOutputHandler getOldTransletOutputHandler() throws IOException {
+	DefaultSAXOutputHandler saxHandler =
+	    new DefaultSAXOutputHandler(System.out, _encoding);
+	return new TextOutput((ContentHandler)saxHandler,
+			      (LexicalHandler)saxHandler, _encoding);
+    }
+}
