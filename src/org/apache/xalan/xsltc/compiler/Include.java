1b76ba345b5bf4b7e7d42c2032be87ab373d8580
Santiago Pericas-Geertsen <santiagopg@apache.org>
2002-07-17 18:52:20 +0000

    Fix for Bugzilla 9171.
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@336575 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/xsltc/compiler/Include.java
+++ src/org/apache/xalan/xsltc/compiler/Include.java
@@ -84,11 +84,12 @@ final class Include extends TopLevelElement {
     private Stylesheet _included = null;
 
     public Stylesheet getIncludedStylesheet() {
-	return(_included);
+	return _included;
     }
 
     public void parseContents(final Parser parser) {
 	final Stylesheet context = parser.getCurrentStylesheet();
+
 	String docToLoad = getAttribute("href");
 	try {
 	    if (context.checkForLoop(docToLoad)) {
@@ -133,7 +134,7 @@ final class Include extends TopLevelElement {
 
 	    final SyntaxTreeNode root = parser.parse(input);
 	    if (root == null) return;
-	    final Stylesheet _included = parser.makeStylesheet(root);
+	    _included = parser.makeStylesheet(root);
 	    if (_included == null) return;
 
 	    _included.setSourceLoader(loader);
@@ -146,7 +147,6 @@ final class Include extends TopLevelElement {
 	    // as the stylesheet that included it.
 	    final int precedence = context.getImportPrecedence();
 	    _included.setImportPrecedence(precedence);
-
 	    parser.setCurrentStylesheet(_included);
 	    _included.parseContents(parser);
 
@@ -155,12 +155,15 @@ final class Include extends TopLevelElement {
 	    while (elements.hasMoreElements()) {
 		final Object element = elements.nextElement();
 		if (element instanceof TopLevelElement) {
-		    if (element instanceof Variable)
-			topStylesheet.addVariable((Variable)element);
-		    else if (element instanceof Param)
-			topStylesheet.addParam((Param)element);
-		    else
-			topStylesheet.addElement((TopLevelElement)element);
+		    if (element instanceof Variable) {
+			topStylesheet.addVariable((Variable) element);
+		    }
+		    else if (element instanceof Param) {
+			topStylesheet.addParam((Param) element);
+		    }
+		    else {
+			topStylesheet.addElement((TopLevelElement) element);
+		    }
 		}
 	    }
 	}
