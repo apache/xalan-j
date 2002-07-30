8a0be581489aee1f3ee9ff50edb28c82ee03398e
tmiller <dev-null@apache.org>
2002-07-30 22:07:52 +0000

    improved selection algorithm for overloaded methods and constructors in ext java funct calls
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@336611 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/xsltc/compiler/util/MultiHashtable.java
+++ src/org/apache/xalan/xsltc/compiler/util/MultiHashtable.java
@@ -75,17 +75,18 @@ public final class MultiHashtable extends Hashtable {
 	return vector;
     }
 	
-    public boolean maps(Object from, Object to) {
-	if (from == null) return false;
+    public Object maps(Object from, Object to) {
+	if (from == null) return null;
 	final Vector vector = (Vector) get(from);
 	if (vector != null) {
 	    final int n = vector.size();
 	    for (int i = 0; i < n; i++) {
-		if (vector.elementAt(i).equals(to)) {
-		    return true;
+                final Object item = vector.elementAt(i);
+		if (item.equals(to)) {
+		    return item;
 		}
 	    }
 	}
-	return false;
+	return null;
     }
 }
