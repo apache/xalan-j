4d05015db171d008d1b7df3d3c2f0215be918b64
Myriam Midy <mmidy@apache.org>
2002-10-17 20:11:02 +0000

    Add function deep-equal
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/branches/xslt20@336883 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xpath/functions/FuncDeepEqual.java
@@ -0,0 +1,258 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, Lotus
+ * Development Corporation., http://www.lotus.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.xpath.functions;
+
+//import org.w3c.dom.Node;
+//import org.w3c.dom.traversal.NodeIterator;
+import org.apache.xml.utils.DateTimeObj;
+import org.apache.xpath.XPathContext;
+import org.apache.xpath.objects.XObject;
+import org.apache.xpath.objects.XSequence;
+import org.apache.xpath.objects.XSequenceImpl;
+import org.apache.xpath.objects.XString;
+import org.apache.xpath.objects.XNodeSequenceSingleton;
+import org.apache.xpath.objects.XBoolean;
+import java.util.Comparator;
+import org.apache.xml.dtm.XType;
+import org.apache.xml.dtm.DTM;
+import org.apache.xpath.parser.regexp.*;
+import org.apache.xalan.res.XSLMessages;
+import org.apache.xpath.res.XPATHErrorResources;
+import org.apache.xpath.XPathException;
+
+import java.text.Collator;
+import java.net.URL;
+
+/**
+ * <meta name="usage" content="advanced"/>
+ * Execute the xs:matches() function.
+ */
+public class FuncDeepEqual extends Function3Args
+{
+
+  /**
+   * Execute the function.  The function must return
+   * a valid object.
+   * @param xctxt The current execution context.
+   * @return A valid XObject.
+   *
+   * @throws javax.xml.transform.TransformerException
+   */
+  public XObject execute(XPathContext xctxt) throws javax.xml.transform.TransformerException
+  {
+    XSequence seq1 = m_arg0.execute(xctxt).xseq();
+    XSequence seq2 = m_arg1.execute(xctxt).xseq();
+    
+    if (seq1.getLength() != seq2.getLength())
+      return new XBoolean(false);	
+    
+    java.text.Collator collator=null;
+    if(null != m_arg2)
+    {
+      String collation=m_arg2.execute(xctxt).str();
+      
+      // We can handle this special case...
+      if(collation!=null && 
+         collation.equals(xctxt.getDefaultCollation()))
+        collator=xctxt.getDefaultCollator();
+
+      // But I currently have no clue what to do with others.
+      // %REVIEW%
+      else
+      {
+        // This should probably be error rather than 
+        // exception -- %REVIEW%
+        throw new XPathException(XSLMessages.createXPATHMessage(
+          XPATHErrorResources.ER_CANNOT_FIND_COLLATOR,
+          new Object[]{collation}
+          ) );
+      }
+    }
+    else // Unspecified, take the static default
+      collator=xctxt.getDefaultCollator();
+
+    XObject item1, item2;
+    
+    if((item1 = seq1.next()) != null &&(item2 = seq2.next()) != null )  	
+    {
+      int type = item1.getType();
+      if(type == XType.NODE)
+      {
+        if(item1 instanceof XNodeSequenceSingleton)
+        {
+          XNodeSequenceSingleton xnss1 = (XNodeSequenceSingleton)item1;
+          XNodeSequenceSingleton xnss2 = (XNodeSequenceSingleton)item2;
+          //%Review% Do I really need 2 dtms??
+          DTM dtm1 = xnss1.getDTM();
+          DTM dtm2 = xnss2.getDTM();
+          int node1 = xnss1.getNodeHandle();
+          int node2 = xnss2.getNodeHandle();
+          
+          if (xnss1.deepEquals(xnss2))
+          {
+            return new XBoolean(deepEqual(node1, node2, dtm1, dtm2, collator));
+          }
+          else
+            return new XBoolean(false);
+          
+        }
+      }
+    }
+    return new XBoolean(false);
+    
+  }
+  
+  private boolean deepEqual(int node1, int node2, DTM dtm1, DTM dtm2, java.text.Collator collator)
+  {
+    int type1 = dtm1.getNodeType(node1);
+    if (type1 == dtm2.getNodeType(node2)
+        && dtm1.getNodeName(node1).equals(dtm2.getNodeName(node2)))
+    {
+      switch (type1)
+      {
+      case DTM.COMMENT_NODE:
+      case DTM.TEXT_NODE:
+      case DTM.NAMESPACE_NODE:
+     case DTM.PROCESSING_INSTRUCTION_NODE:
+      if (collator != null)
+      {
+        if (collator.equals(dtm1.getNodeValue(node1), dtm2.getNodeValue(node2)))
+          return true;
+        else 
+          return false;
+      }
+      else
+      {
+        if (dtm1.getNodeValue(node1).equals(dtm2.getNodeValue(node2)))
+          return true;
+        else 
+          return false;
+      }
+      }
+      
+      {
+        int attrNode1 = dtm1.getFirstAttribute(node1);
+        int attrNode2 = dtm2.getFirstAttribute(node2);
+        while (attrNode1 != DTM.NULL && attrNode2 != DTM.NULL)
+        {
+          attrNode2 = dtm2.getAttributeNode(node2, dtm1.getNamespaceURI(attrNode1), dtm1.getNodeName(attrNode1));	             
+         if (attrNode2 == DTM.NULL)
+          return false; 
+         if (deepEqual(attrNode1, attrNode2, dtm1, dtm2, collator))
+            //collator.equals(dtm1.getNodeValue(attrNode1), dtm2.getNodeValue(node2))) 
+          {	                 
+            attrNode1 = dtm1.getNextAttribute(attrNode1);
+            //attrNode2 = dtm2.getNextAttribute(attrNode2);
+          }
+         else
+           break;
+        }
+        if (attrNode1 != DTM.NULL)
+          return false;  
+        
+        if (dtm1.hasChildNodes(node1) && dtm2.hasChildNodes(node2))
+        {
+          int child1 = dtm1.getFirstChild(node1);
+          int child2 = dtm2.getFirstChild(node2);
+          while (deepEqual(child1, child2, dtm1, dtm2, collator))
+          {
+            child1 = dtm1.getNextSibling(child1);
+            child2 = dtm2.getNextSibling(child2);
+            if (child1 == DTM.NULL || child2 == DTM.NULL)
+              break;
+          }
+          if (child1 != DTM.NULL && child2 != DTM.NULL)
+            return false;
+        }
+        
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  /**
+   * Check that the number of arguments passed to this function is correct. 
+   *
+   *
+   * @param argNum The number of arguments that is being passed to the function.
+   *
+   * @throws WrongNumberArgsException
+   */
+  public void checkNumberArgs(int argNum) throws WrongNumberArgsException
+  {
+    if (argNum < 2 || argNum > 3)
+      reportWrongNumberArgs();
+  }
+
+  /**
+   * Constructs and throws a WrongNumberArgException with the appropriate
+   * message for this function object.
+   *
+   * @throws WrongNumberArgsException
+   */
+  protected void reportWrongNumberArgs() throws WrongNumberArgsException {
+      throw new WrongNumberArgsException(XSLMessages.createXPATHMessage("twoorthree", null));
+  }
+  
+  
+  /** Return the number of children the node has. */
+  public int exprGetNumChildren()
+  {
+  	return (m_arg2 == null) ?  2 :  3;
+  }
+}
