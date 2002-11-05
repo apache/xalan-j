4be5e2ef0908bd3a0d8f74c867dda506ea85ad41
Henry Zongaro <zongaro@apache.org>
2002-11-05 21:01:34 +0000

    A very simple class to act as an adapter from the ContentHandler interface to the TransletOutputHandler interface.  Right now only the characters, startCDATA, endCDATA and comments methods are implemented.
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/branches/XSLTC_DTM@336969 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xalan/xsltc/dom/CH2TOH.java
@@ -0,0 +1,197 @@
+/*
+ * @(#)$Id$
+ *
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Sun
+ * Microsystems., http://www.sun.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.xalan.xsltc.dom;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.Attributes;
+import org.xml.sax.ext.LexicalHandler;
+
+import org.apache.xalan.xsltc.TransletException;
+import org.apache.xalan.xsltc.TransletOutputHandler;
+import org.apache.xalan.xsltc.runtime.BasisLibrary;
+
+/**
+ *
+ * A special purpose class used to convert SAX events to TransletOutputHandler
+ * events.  This should only be needed temporarily, until the Xalan interpretive
+ * and XSLTC serializers become more closely coupled.
+ *
+ * Currently it only handles characters, comment, startCDATA and endCDATA
+ * events.
+ *
+ */
+final class CH2TOH implements ContentHandler, LexicalHandler
+{
+    private TransletOutputHandler m_TOH;
+
+    public void setTOH(TransletOutputHandler TOH) {
+        m_TOH = TOH;
+    }
+
+    public void setDocumentLocator(Locator locator) {}
+
+    public void startDocument() throws SAXException {
+        unimplemented();
+    }
+
+    public void endDocument() throws SAXException {
+        unimplemented();
+    }
+
+    public void startPrefixMapping(String prefix, String uri)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void endPrefixMapping(String prefix) throws SAXException {
+        unimplemented();
+    }
+
+    public void startElement(String namespaceURI, String localName,
+                             String qName, Attributes atts)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void endElement(String namespaceURI, String localName,
+                           String qName)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void skippedEntity (String name) throws SAXException {
+        unimplemented();
+    }
+
+    public void processingInstruction (String target, String data)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void ignorableWhitespace (char ch[], int start, int length)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void characters(char[] ch, int offset, int length)
+        throws SAXException
+    {
+        try {
+            m_TOH.characters(ch, offset, length);
+        } catch (TransletException e) {
+            throw new SAXException(e);
+        }
+    }
+
+    public void startDTD(String name, String publicId, String systemId)
+        throws SAXException
+    {
+        unimplemented();
+    }
+
+    public void endDTD() throws SAXException {
+        unimplemented();
+    }
+
+    public void startEntity(String name) throws SAXException {
+        unimplemented();
+    }
+
+    public void endEntity(String name) throws SAXException {
+        unimplemented();
+    }
+
+    public void startCDATA() throws SAXException {
+        try {
+            m_TOH.startCDATA();
+        } catch (TransletException e) {
+            throw new SAXException(e);
+        }
+    }
+
+    public void endCDATA() throws SAXException {
+        try {
+            m_TOH.endCDATA();
+        } catch (TransletException e) {
+            throw new SAXException(e);
+        }
+    }
+
+    public void comment(char ch[], int start, int length)
+        throws SAXException
+    {
+        try {
+            m_TOH.comment(new String(ch, start, length));
+        } catch (TransletException e) {
+            throw new SAXException(e);
+        }
+    }
+
+    private void unimplemented() {
+        BasisLibrary.runTimeError(BasisLibrary.RUN_TIME_INTERNAL_ERR, "CH2TOH");
+    }
+}
