7afbe34f4a0deb6d2f13e6901e3fc8bb87b20661
Morris Kwan <mkwan@apache.org>
2002-11-15 21:15:28 +0000

    Code cleanup. Remove all the unnecessary imports.
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@337003 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/lib/ExsltCommon.java
+++ src/org/apache/xalan/lib/ExsltCommon.java
@@ -56,38 +56,10 @@
  */
 package org.apache.xalan.lib;
 
-import org.w3c.dom.Node;
-import org.w3c.dom.Document;
-import org.w3c.dom.DocumentFragment;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.w3c.dom.traversal.NodeIterator;
-
-import org.apache.xpath.NodeSet;
-import org.apache.xpath.objects.XObject;
-import org.apache.xpath.objects.XBoolean;
-import org.apache.xpath.objects.XNumber;
-import org.apache.xpath.objects.XRTreeFrag;
-
-import org.apache.xpath.XPath;
-import org.apache.xpath.XPathContext;
-import org.apache.xpath.DOMHelper;
 import org.apache.xml.dtm.DTMIterator;
-import org.apache.xml.dtm.DTM;
 import org.apache.xml.dtm.ref.DTMNodeIterator;
-import org.apache.xml.utils.XMLString;
-
-import org.xml.sax.SAXNotSupportedException;
-
-import java.util.Hashtable;
-import java.util.StringTokenizer;
-
+import org.apache.xpath.NodeSet;
 import org.apache.xalan.extensions.ExpressionContext;
-import org.apache.xalan.res.XSLMessages;
-import org.apache.xalan.res.XSLTErrorResources;
-import org.apache.xalan.xslt.EnvironmentCheck;
-
-import javax.xml.parsers.*;
 
 /**
  * <meta name="usage" content="general"/>
