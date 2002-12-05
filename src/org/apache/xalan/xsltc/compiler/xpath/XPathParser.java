f6952b0319cff46c895925904720f627c65ad8af
Santiago Pericas-Geertsen <santiagopg@apache.org>
2002-12-05 20:27:16 +0000

    Simple test for XPath 2.0 parser.
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/branches/xslt20@337041 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/xalan/xsltc/compiler/xpath/XPathParser.java
@@ -0,0 +1,105 @@
+/*
+ * @(#)$Id$
+ *
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Sun
+ * Microsystems., http://www.sun.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * @author Santiago Pericas-Geertsen
+ *
+ */
+
+package org.apache.xalan.xsltc.compiler.xpath;
+
+import java.io.StringReader;
+import org.apache.xpath.parser.*;
+
+public class XPathParser {
+
+    private XPath _xpath = null;
+
+    public XPathParser() {
+    }
+
+    private void initXPath(String s) {
+        if (_xpath == null) {
+            _xpath = new XPath(new StringReader(s));
+        }
+        else {
+            _xpath.ReInit(new StringReader(s));
+        }
+    }
+
+    SimpleNode parseExpression(String expression) throws ParseException {
+        initXPath(expression);
+	return _xpath.XPath2();
+    }
+
+    SimpleNode parserPattern(String pattern) throws ParseException {
+        initXPath(pattern);
+	return _xpath.MatchPattern();
+    }
+
+    public static void main(String[] args) {
+        try {
+            Node tree;
+            System.out.println("AST = " +
+              (tree = new XPathParser().parseExpression("a/b")));
+            ((SimpleNode)tree.jjtGetChild(0)).dump("|") ;
+            System.out.println("done");
+        }
+        catch (Exception e) {
+        }
+    }
+}
