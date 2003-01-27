96767ebe0fcebaf8c386a834f136b5c3517ba5f6
Morris Kwan <mkwan@apache.org>
2003-01-27 19:46:38 +0000

    Commit the copyright patches from Sarah Mcnamara (mcnamara@ca.ibm.com).
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/branches/XSLTC_DTM@337146 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/extensions/MethodResolver.java
+++ src/org/apache/xalan/extensions/MethodResolver.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * Copyright (c) 1999-2003 The Apache Software Foundation.  All rights 
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -54,7 +54,6 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-
 package org.apache.xalan.extensions;
 
 import java.lang.reflect.Method;
