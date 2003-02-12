4ef5d8830fd244df55b884d2cb2e7e10d0d9ea18
Santiago Pericas-Geertsen <santiagopg@apache.org>
2003-02-12 16:15:41 +0000

    Implementation of default node factory.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/branches/xslt20@337238 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xpath_rwapi/src/org/apache/xpath/rwapi/impl/parser/DefaultNodeFactory.java
@@ -0,0 +1,137 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, Lotus
+ * Development Corporation., http://www.lotus.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.xpath.rwapi.impl.parser;
+
+import org.apache.xpath.rwapi.impl.FunctionCallImpl;
+import org.apache.xpath.rwapi.impl.KindTestImpl;
+import org.apache.xpath.rwapi.impl.LiteralImpl;
+import org.apache.xpath.rwapi.impl.NameTestImpl;
+import org.apache.xpath.rwapi.impl.OperatorImpl;
+import org.apache.xpath.rwapi.impl.PathExprImpl;
+import org.apache.xpath.rwapi.impl.StepExprImpl;
+import org.apache.xpath.rwapi.impl.VariableImpl;
+
+/**
+ * Default implementation for node factory
+ */
+public class DefaultNodeFactory implements NodeFactory {
+
+	
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createFunctionCallNode()
+	 */
+	public FunctionCallImpl createFunctionCallNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createKindTestNode(int)
+	 */
+	public KindTestImpl createKindTestNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createLiteralNode(int)
+	 */
+	public LiteralImpl createLiteralNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createNameTestNode()
+	 */
+	public NameTestImpl createNameTestNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createNode(int)
+	 */
+	public Node createNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createOperatorNode(int)
+	 */
+	public OperatorImpl createOperatorNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createPathNode()
+	 */
+	public PathExprImpl createPathNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createStepNode()
+	 */
+	public StepExprImpl createStepNode(int id) {
+		return null;
+	}
+
+	/**
+	 * @see org.apache.xpath.rwapi.impl.parser.NodeFactory#createVarNameNode()
+	 */
+	public VariableImpl createVarNameNode(int id) {
+		return null;
+	}
+
+}
