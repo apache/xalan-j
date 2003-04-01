932298141a8e792c21b8b7d7b09d7e4a74c59f95
Morris Kwan <mkwan@apache.org>
2003-04-01 20:35:37 +0000

    Merging XSLTC_DTM and common serializer to the head
    Changes in org.apache.xalan.xsltc.
    
    1. Remove TransletOutputHandler.java
    2. Adapt the XSLTC interfaces to the DTM model.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@337389 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/xsltc/Translet.java
+++ src/org/apache/xalan/xsltc/Translet.java
@@ -63,20 +63,23 @@
 
 package org.apache.xalan.xsltc;
 
+import org.apache.xml.dtm.DTMAxisIterator;
+import org.apache.xml.serializer.SerializationHandler;
+
 public interface Translet {
 
-    public void transform(DOM document, TransletOutputHandler handler)
+    public void transform(DOM document, SerializationHandler handler)
 	throws TransletException;
-    public void transform(DOM document, TransletOutputHandler[] handlers)
+    public void transform(DOM document, SerializationHandler[] handlers)
 	throws TransletException;
-    public void transform(DOM document, NodeIterator iterator,
-			  TransletOutputHandler handler)
+    public void transform(DOM document, DTMAxisIterator iterator,
+			  SerializationHandler handler)
 	throws TransletException;
 
     public Object addParameter(String name, Object value);
 
-    public void buildKeys(DOM document, NodeIterator iterator,
-			  TransletOutputHandler handler, int root)
+    public void buildKeys(DOM document, DTMAxisIterator iterator,
+			  SerializationHandler handler, int root)
 	throws TransletException;
     public void addAuxiliaryClass(Class auxClass);
     public Class getAuxiliaryClass(String className);
