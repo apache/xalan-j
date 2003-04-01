a33569fd081362226c801adb9b79ce60972dd530
Morris Kwan <mkwan@apache.org>
2003-04-01 18:58:12 +0000

    Merging XSLTC_DTM and common serializer to the head
    Changes in org.apache.xml.dtm.ref.dom2dtm.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@337375 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
+++ src/org/apache/xml/dtm/ref/dom2dtm/DOM2DTM.java
@@ -346,7 +346,7 @@ public class DOM2DTM extends DTMDefaultBaseIterators
   /**
    * Get the number of nodes that have been added.
    */
-  protected int getNumberOfNodes()
+  public int getNumberOfNodes()
   {
     return m_nodes.size();
   }
@@ -891,6 +891,39 @@ public class DOM2DTM extends DTMDefaultBaseIterators
     }
     else
       return m_xstrf.newstr( node.getNodeValue() );
+  }
+  
+  /**
+   * Determine if the string-value of a node is whitespace
+   *
+   * @param nodeHandle The node Handle.
+   *
+   * @return Return true if the given node is whitespace.
+   */
+  public boolean isWhitespace(int nodeHandle)
+  {
+  	int type = getNodeType(nodeHandle);
+    Node node = getNode(nodeHandle);
+  	if(TEXT_NODE == type || CDATA_SECTION_NODE == type)
+    {
+      // If this is a DTM text node, it may be made of multiple DOM text
+      // nodes -- including navigating into Entity References. DOM2DTM
+      // records the first node in the sequence and requires that we
+      // pick up the others when we retrieve the DTM node's value.
+      //
+      // %REVIEW% DOM Level 3 is expected to add a "whole text"
+      // retrieval method which performs this function for us.
+      FastStringBuffer buf = StringBufferPool.get();
+      while(node!=null)
+      {
+        buf.append(node.getNodeValue());
+        node=logicalNextDOMTextNode(node);
+      }
+     boolean b = buf.isWhitespace(0, buf.length());
+      StringBufferPool.free(buf);
+     return b;
+    }
+    return false;
   }
   
   /**
@@ -1768,6 +1801,7 @@ public class DOM2DTM extends DTMDefaultBaseIterators
   {
     return null;
   }
+
 }
 
 
