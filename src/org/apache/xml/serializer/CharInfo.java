f9fe593c91d5342ade4af385e88a9f0a430e142a
Henry Zongaro <zongaro@apache.org>
2003-04-06 20:54:06 +0000

    Patch from Christine Li (jycli@ca.ibm.com).  Changed CharInfo so that it allows descriptions of entities to be specified as a properties file in addition to a Xalan-specific resource file format.
    The code previously always assumed a Xalan-specific resource file format, for
    both user-supplied descriptions of entities, and for those supplied with
    Xalan-J.  The security restrictions of applets are such that an applet that
    relies on a copy of Xalan-J in the Java run-time would find that Xalan-J would
    be unable to read the default entities file that is packaged with the JRE on
    the applet's behalf - it can only do so with a properties, via ResourceBundle.
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@337415 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/serializer/CharInfo.java
+++ src/org/apache/xml/serializer/CharInfo.java
@@ -60,15 +60,20 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.MalformedURLException;
 import java.util.BitSet;
 import java.util.Hashtable;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.Enumeration;
 
 import org.apache.xml.res.XMLErrorResources;
 import org.apache.xml.res.XMLMessages;
 import org.apache.xml.utils.CharKey;
 import org.apache.xml.utils.SystemIDResolver;
 import org.apache.xml.utils.SystemIDResolver;
-
 /**
  * This class provides services that tell if a character should have
  * special treatement, such as entity reference substitution or normalization
@@ -90,13 +95,13 @@ public class CharInfo
      * The name of the HTML entities file.
      * If specified, the file will be resource loaded with the default class loader.
      */
-    public static String HTML_ENTITIES_RESOURCE = "HTMLEntities.res";
+    public static String HTML_ENTITIES_RESOURCE = "org.apache.xml.serializer.HTMLEntities";
 
     /**
      * The name of the XML entities file.
      * If specified, the file will be resource loaded with the default class loader.
      */
-    public static String XML_ENTITIES_RESOURCE = "XMLEntities.res";
+    public static String XML_ENTITIES_RESOURCE = "org.apache.xml.serializer.XMLEntities";
 
     /** The linefeed character, which the parser should always normalize. */
     public static final char S_LINEFEED = 0x0A;
@@ -108,21 +113,24 @@ public class CharInfo
      * Constructor that reads in a resource file that describes the mapping of
      * characters to entity references.
      *
-     * Resource files must be encoded in UTF-8 and have a format like:
+     * Resource files must be encoded in UTF-8 and can either be properties
+     * files with a .properties extension assumed.  Alternatively, they can
+     * have the following form, with no particular extension assumed:
+     *
      * <pre>
      * # First char # is a comment
      * Entity numericValue
      * quot 34
      * amp 38
      * </pre>
-     * (Note: Why don't we just switch to .properties files? Oct-01 -sc)
      *
-     * @param entitiesResource Name of entities resource file that should
-     * be loaded, which describes that mapping of characters to entity references.
+     * @param entitiesResource Name of properties or resource file that should
+     * be loaded, which describes that mapping of characters to entity
+     * references.
      */
     public CharInfo(String entitiesResource)
     {
-
+        PropertyResourceBundle entities;
         InputStream is = null;
         BufferedReader reader = null;
         int index;
@@ -131,132 +139,134 @@ public class CharInfo
         int code;
         String line;
 
-        try
-        {
-            try
-            {
-                // Maintenance note: we should evaluate replacing getting the 
-                //  ClassLoader with javax.xml.transform.FactoryFinder.findClassLoader()
-                //  or similar code
-                ClassLoader cl = CharInfo.class.getClassLoader();
-
-                if (cl == null)
-                {
-                    is =
-                        ClassLoader.getSystemResourceAsStream(entitiesResource);
-                }
-                else
-                {
-                    is = cl.getResourceAsStream(entitiesResource);
-                }
+        // Make various attempts to interpret the parameter as a properties
+        // file or resource file, as follows:
+        //
+        //   1) attempt to load .properties file using ResourceBundle
+        //   2) try using the class loader to find the specified file a resource
+        //      file
+        //   3) try treating the resource a URI
+        entities = loadEntitiesResource(entitiesResource);
+        if (null != entities) {
+            Enumeration enum = entities.getKeys();
+            while (enum.hasMoreElements()){
+                name = (String) enum.nextElement();
+                value = entities.getString(name);
+                code = Integer.parseInt(value);
+                defineEntity(name, (char) code);
             }
-            catch (Exception e)
-            {
-            }
+            m_specialsMap.set(S_LINEFEED);
+            m_specialsMap.set(S_CARRIAGERETURN);
+        } else {
+            // Load user specified resource file by using URL loading, it
+            // requires a valid URI as parameter;                
+            try {
+                try {
+                    // Maintenance note: we should evaluate replacing getting
+                    // the ClassLoader with
+                    // javax.xml.transform.FactoryFinder.findClassLoader()
+                    // or similar code
+                    ClassLoader cl = CharInfo.class.getClassLoader();
 
-            if (is == null)
-                is = CharInfo.class.getResourceAsStream(entitiesResource);
-
-            if (is == null)
-            {
-                URL url = new URL(entitiesResource);
-
-                is = url.openStream();
-            }
-
-            if (is == null)
-                throw new RuntimeException(
-                    XMLMessages.createXMLMessage(
-                        XMLErrorResources.ER_RESOURCE_COULD_NOT_FIND,
-                        new Object[] { entitiesResource, entitiesResource }));
-
-            // Fix Bugzilla#4000: force reading in UTF-8
-            //  This creates the de facto standard that Xalan's resource 
-            //  files must be encoded in UTF-8. This should work in all JVMs.
-            //
-            // %REVIEW% KNOWN ISSUE: IT FAILS IN MICROSOFT VJ++, which
-            // didn't implement the UTF-8 encoding. Theoretically, we should
-            // simply let it fail in that case, since the JVM is obviously
-            // broken if it doesn't support such a basic standard.  But
-            // since there are still some users attempting to use VJ++ for
-            // development, we have dropped in a fallback which makes a
-            // second attempt using the platform's default encoding. In VJ++
-            // this is apparently ASCII, which is subset of UTF-8... and
-            // since the strings we'll be reading here are also primarily
-            // limited to the 7-bit ASCII range (at least, in English
-            // versions of Xalan), this should work well enough to keep us
-            // on the air until we're ready to officially decommit from
-            // VJ++.
-            try
-            {
-                reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            }
-            catch (java.io.UnsupportedEncodingException e)
-            {
-                reader = new BufferedReader(new InputStreamReader(is));
-            }
-
-            line = reader.readLine();
-
-            while (line != null)
-            {
-                if (line.length() == 0 || line.charAt(0) == '#')
-                {
-                    line = reader.readLine();
-
-                    continue;
-                }
-
-                index = line.indexOf(' ');
-
-                if (index > 1)
-                {
-                    name = line.substring(0, index);
-
-                    ++index;
-
-                    if (index < line.length())
-                    {
-                        value = line.substring(index);
-                        index = value.indexOf(' ');
-
-                        if (index > 0)
-                            value = value.substring(0, index);
-
-                        code = Integer.parseInt(value);
-
-                        defineEntity(name, (char) code);
+                    if (cl == null) {
+                        is = ClassLoader.getSystemResourceAsStream(
+                                                             entitiesResource);
+                    } else {
+                        is = cl.getResourceAsStream(entitiesResource);
                     }
+                } catch (Exception e) { }
+
+                if (is == null) {
+                    is = CharInfo.class.getResourceAsStream(entitiesResource);
+                }
+
+                if (is == null) {
+                    URL url = new URL(entitiesResource);
+
+                    is = url.openStream();
+                }
+
+                if (is == null) {
+                    throw new RuntimeException(
+                        XMLMessages.createXMLMessage(
+                            XMLErrorResources.ER_RESOURCE_COULD_NOT_FIND,
+                            new Object[] {entitiesResource, entitiesResource}));
+                }
+
+                // Fix Bugzilla#4000: force reading in UTF-8
+                //  This creates the de facto standard that Xalan's resource 
+                //  files must be encoded in UTF-8. This should work in all
+                // JVMs.
+                //
+                // %REVIEW% KNOWN ISSUE: IT FAILS IN MICROSOFT VJ++, which
+                // didn't implement the UTF-8 encoding. Theoretically, we should
+                // simply let it fail in that case, since the JVM is obviously
+                // broken if it doesn't support such a basic standard.  But
+                // since there are still some users attempting to use VJ++ for
+                // development, we have dropped in a fallback which makes a
+                // second attempt using the platform's default encoding. In VJ++
+                // this is apparently ASCII, which is subset of UTF-8... and
+                // since the strings we'll be reading here are also primarily
+                // limited to the 7-bit ASCII range (at least, in English
+                // versions of Xalan), this should work well enough to keep us
+                // on the air until we're ready to officially decommit from
+                // VJ++.
+                try {
+                    reader = new BufferedReader(new InputStreamReader(is,
+                                                                      "UTF-8"));
+                } catch (java.io.UnsupportedEncodingException e) {
+                    reader = new BufferedReader(new InputStreamReader(is));
                 }
 
                 line = reader.readLine();
-            }
 
-            is.close();
-            m_specialsMap.set(S_LINEFEED);
-            m_specialsMap.set(S_CARRIAGERETURN);
-        }
-        catch (Exception except)
-        {
-            throw new RuntimeException(
-                XMLMessages.createXMLMessage(
-                    XMLErrorResources.ER_RESOURCE_COULD_NOT_LOAD,
-                    new Object[] {
-                        entitiesResource,
-                        except.toString(),
-                        entitiesResource,
-                        except.toString()}));
-        }
-        finally
-        {
-            if (is != null)
-            {
-                try
-                {
-                    is.close();
+                while (line != null) {
+                    if (line.length() == 0 || line.charAt(0) == '#') {
+                        line = reader.readLine();
+
+                        continue;
+                    }
+
+                    index = line.indexOf(' ');
+
+                    if (index > 1) {
+                        name = line.substring(0, index);
+
+                        ++index;
+
+                        if (index < line.length()) {
+                            value = line.substring(index);
+                            index = value.indexOf(' ');
+
+                            if (index > 0) {
+                                value = value.substring(0, index);
+                            }
+
+                            code = Integer.parseInt(value);
+
+                            defineEntity(name, (char) code);
+                        }
+                    }
+
+                    line = reader.readLine();
                 }
-                catch (Exception except)
-                {
+
+                is.close();
+                m_specialsMap.set(S_LINEFEED);
+                m_specialsMap.set(S_CARRIAGERETURN);
+            } catch (Exception except) {
+                throw new RuntimeException(
+                    XMLMessages.createXMLMessage(
+                        XMLErrorResources.ER_RESOURCE_COULD_NOT_LOAD,
+                        new Object[] { entitiesResource,
+                                       except.toString(),
+                                       entitiesResource,
+                                       except.toString()}));
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (Exception except) { }
                 }
             }
         }
@@ -394,4 +404,21 @@ public class CharInfo
         }
         return retobj;
     }
+    
+//Load entity property files by using PropertyResourceBundle, cause of security issure for applets
+        private PropertyResourceBundle loadEntitiesResource(String baseName)
+                                throws MissingResourceException
+        {    
+                try
+                {
+                        Locale locale = Locale.getDefault();
+                        java.lang.ClassLoader loader = this.getClass().getClassLoader(); 
+                        return (PropertyResourceBundle)PropertyResourceBundle.getBundle(baseName);
+                }
+                catch (MissingResourceException e)
+                {
+                        return null;
+                }
+        }
+    
 }
