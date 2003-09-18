4e34d548374cd4acaa85ed24c789b09e874620df
Brian James Minchau <minchau@apache.org>
2003-09-18 19:18:53 +0000

    PR: Bugzilla 23089 Submitted by:	Joanne Tong Reviewed by:	Brian Minchau
    A slight rework of Joanne's patch made by Brian and now commited to CVS.
     Also just previosly commited was a testcase, extend06 created by Joanne (thank!).
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@337646 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/processor/ProcessorUnknown.java
+++ src/org/apache/xalan/processor/ProcessorUnknown.java
@@ -60,42 +60,12 @@ import org.xml.sax.Attributes;
 
 /**
  * <meta name="usage" content="internal"/>
- * This class processes an unknown template element, and ignores 
- * the startElement and endElement events.  It is used both 
+ * This class processes an unknown template element.  It is used both 
  * for unknown top-level elements, and for elements in the 
  * xslt namespace when the version is higher than the version 
  * of XSLT that we are set up to process.
  */
-public class ProcessorUnknown extends ProcessorTemplateElem
+public class ProcessorUnknown extends ProcessorLRE
 {
 
-  /**
-   * Receive notification of the start of an element.
-   *
-   * @param name The element type name.
-   *
-   * @param handler non-null reference to current StylesheetHandler that is constructing the Templates.
-   * @param uri The Namespace URI, or an empty string.
-   * @param localName The local name (without prefix), or empty string if not namespace processing.
-   * @param rawName The qualified name (with prefix).
-   * @param attributes The specified or defaulted attributes.
-   */
-  public void startElement(
-          StylesheetHandler handler, String uri, String localName, String rawName, Attributes attributes)
-            throws org.xml.sax.SAXException{}
-
-  /**
-   * Receive notification of the end of an element.
-   *
-   * @param name The element type name.
-   * @param attributes The specified or defaulted attributes.
-   *
-   * @param handler non-null reference to current StylesheetHandler that is constructing the Templates.
-   * @param uri The Namespace URI, or an empty string.
-   * @param localName The local name (without prefix), or empty string if not namespace processing.
-   * @param rawName The qualified name (with prefix).
-   */
-  public void endElement(
-          StylesheetHandler handler, String uri, String localName, String rawName)
-            throws org.xml.sax.SAXException{}
 }
--- src/org/apache/xalan/templates/ElemUnknown.java
+++ src/org/apache/xalan/templates/ElemUnknown.java
@@ -57,18 +57,32 @@
 package org.apache.xalan.templates;
 
 import javax.xml.transform.TransformerException;
-
+import org.apache.xalan.res.XSLMessages;
+import org.apache.xalan.res.XSLTErrorResources;
 import org.apache.xalan.transformer.TransformerImpl;
+import org.apache.xpath.XPathContext;
+
 
 /**
  * <meta name="usage" content="advanced"/>
- * Implement a Literal Result Element.
+ * Implement an unknown element
  */
 public class ElemUnknown extends ElemLiteralResult
 {
 
   /**
-   * Copy an unknown element to the result tree
+   * Get an int constant identifying the type of element.
+   * @see org.apache.xalan.templates.Constants
+   *
+   *@return The token ID for this element
+   */
+  public int getXSLToken()
+  {
+    return Constants.ELEMNAME_UNDEFINED;
+  }
+  
+  /**
+   * Execute the fallbacks when an extension is not available.
    *
    * @param transformer non-null reference to the the current transform-time state.
    * @param sourceNode non-null reference to the <a href="http://www.w3.org/TR/xslt#dt-current-node">current source node</a>.
@@ -76,7 +90,78 @@ public class ElemUnknown extends ElemLiteralResult
    *
    * @throws TransformerException
    */
-  public void execute(
+  private void executeFallbacks(
           TransformerImpl transformer)
-            throws TransformerException{}
+            throws TransformerException
+  {
+    for (ElemTemplateElement child = m_firstChild; child != null;
+             child = child.m_nextSibling)
+    {
+      if (child.getXSLToken() == Constants.ELEMNAME_FALLBACK)
+      {
+        try
+        {
+          transformer.pushElemTemplateElement(child);
+          ((ElemFallback) child).executeFallback(transformer);
+        }
+        finally
+        {
+          transformer.popElemTemplateElement();
+        }
+      }
+    }
+
+  }
+  
+  /**
+   * Return true if this extension element has a <xsl:fallback> child element.
+   *
+   * @return true if this extension element has a <xsl:fallback> child element.
+   */
+  private boolean hasFallbackChildren()
+  {
+    for (ElemTemplateElement child = m_firstChild; child != null;
+             child = child.m_nextSibling)
+    {
+      if (child.getXSLToken() == Constants.ELEMNAME_FALLBACK)
+        return true;
+    }
+    
+    return false;
+  }
+
+
+  /**
+   * Execute an unknown element.
+   * Execute fallback if fallback child exists or do nothing
+   *
+   * @param transformer non-null reference to the the current transform-time state.
+   * @param sourceNode non-null reference to the <a href="http://www.w3.org/TR/xslt#dt-current-node">current source node</a>.
+   * @param mode reference, which may be null, to the <a href="http://www.w3.org/TR/xslt#modes">current mode</a>.
+   *
+   * @throws TransformerException
+   */
+  public void execute(TransformerImpl transformer)
+            throws TransformerException
+  {
+
+
+	if (TransformerImpl.S_DEBUG)
+		transformer.getTraceManager().fireTraceEvent(this);
+
+	try {
+
+		if (hasFallbackChildren()) {
+			executeFallbacks(transformer);
+		} else {
+			// do nothing
+		}
+		
+	} catch (TransformerException e) {
+		transformer.getErrorListener().fatalError(e);
+	}
+	if (TransformerImpl.S_DEBUG)
+		transformer.getTraceManager().fireTraceEndEvent(this);
+  }
+
 }
