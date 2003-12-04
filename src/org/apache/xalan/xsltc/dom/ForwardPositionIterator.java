0f850b1e40f74c8e953904b4201880af96df53d4
Henry Zongaro <zongaro@apache.org>
2003-12-04 20:58:33 +0000

    Added comments to describe the situation in which this iterator is used.
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/branches/xslt20-compiled@337787 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/xsltc/dom/ForwardPositionIterator.java
+++ src/org/apache/xalan/xsltc/dom/ForwardPositionIterator.java
@@ -71,6 +71,32 @@ import org.apache.xml.dtm.ref.DTMAxisIteratorBase;
  * a node in document order. It is needed for the case where 
  * a call to position() occurs in the context of an XSLT element
  * such as xsl:for-each, xsl:apply-templates, etc. 
+ *
+ * The getPosition() methods in DTMAxisIterators defined
+ * in DTMDefaultBaseIterators always return the position
+ * in document order, which is backwards for XPath in the
+ * case of the ancestor, ancestor-or-self, previous and
+ * previous-sibling.
+ *
+ * XSLTC implements position() with the
+ * BasisLibrary.positionF() method, and uses the
+ * DTMAxisIterator.isReverse() method to determine
+ * whether the result of getPosition() should be
+ * interpreted as being equal to position().
+ * But when the expression appears in apply-templates of
+ * for-each, the position() function operates in document
+ * order.
+ *
+ * The only effect of the ForwardPositionIterator is to force
+ * the result of isReverse() to false, so that
+ * BasisLibrary.positionF() calculates position() in a way
+ * that's consistent with the context in which the
+ * iterator is being used."
+ *
+ * (Apparently the correction of isReverse() occurs
+ * implicitly, by inheritance. This class also appears
+ * to maintain its own position counter, which seems
+ * redundant.)
  */
 public final class ForwardPositionIterator extends DTMAxisIteratorBase {
 
