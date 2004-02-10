0fd44aafe773f989c0477052fb8d6ffd3054fa25
Christine Li <jycli@apache.org>
2004-02-10 22:28:04 +0000

    Fix for bugzilla bug report 24111.
    The string values for both positive and nagative zero are 0. However,
    a number div 0 returns Infinity, a number div -0 returns -Infinity.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@337839 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xpath/functions/FuncRound.java
+++ src/org/apache/xpath/functions/FuncRound.java
@@ -77,7 +77,11 @@ public class FuncRound extends FunctionOneArg
    */
   public XObject execute(XPathContext xctxt) throws javax.xml.transform.TransformerException
   {
-    return new XNumber(java.lang.Math.floor(m_arg0.execute(xctxt).num()
+          final XObject obj = m_arg0.execute(xctxt);
+          final double val= obj.num();
+          if (val >= -0.5 && val < 0) return new XNumber(-0.0);
+          if (val == 0.0) return new XNumber(val);
+          return new XNumber(java.lang.Math.floor(val
                                             + 0.5));
   }
 }
