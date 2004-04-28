c186d631b6620f6aa552df077f498c685d8d4893
Sarah McNamara <mcnamara@apache.org>
2004-04-28 02:31:11 +0000

    Fix copyright and use @xsl.usage tag in javadocs
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@337968 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xalan/Version.java
+++ src/org/apache/xalan/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2004 The Apache Software Foundation.
+ * Copyright 2001-2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 package org.apache.xalan;
 
 /**
- * <meta name="usage" content="general"/>
  * Administrative class to keep track of the version number of
  * the Xalan release.
  * <P>This class implements the upcoming standard of having
@@ -28,6 +27,7 @@ package org.apache.xalan;
  * org.apache.xalan.processor.Version class.</P>
  * <P>See also: org/apache/xalan/res/XSLTInfo.properties for 
  * information about the version of the XSLT spec we support.</P>
+ * @xsl.usage general
  */
 public class Version
 {
