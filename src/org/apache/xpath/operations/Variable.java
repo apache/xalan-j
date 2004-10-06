ef11ec3b223370fc09efad0584ac9b57239c5462
Brian James Minchau <minchau@apache.org>
2004-10-06 20:04:09 +0000

    Null Pointer Exception on sorting using a variable PR: XALANJ-1942 Submitted by:	Yash Talwar Reviewed by:	Brian Minchau
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@338047 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xpath/operations/Variable.java
+++ src/org/apache/xpath/operations/Variable.java
@@ -260,19 +260,18 @@ public class Variable extends Expression implements PathComponent
     // qname.  If we reach the top level, use the StylesheetRoot's composed
     // list of top level variables and parameters.
     
+    org.apache.xalan.templates.ElemVariable vvar = null;	
     org.apache.xpath.ExpressionNode owner = getExpressionOwner();
 
     if (null != owner && owner instanceof org.apache.xalan.templates.ElemTemplateElement)
     {
-
-      org.apache.xalan.templates.ElemVariable vvar;
 
       org.apache.xalan.templates.ElemTemplateElement prev = 
         (org.apache.xalan.templates.ElemTemplateElement) owner;
 
       if (!(prev instanceof org.apache.xalan.templates.Stylesheet))
       {            
-        while ( !(prev.getParentNode() instanceof org.apache.xalan.templates.Stylesheet) )
+        while ( prev != null && !(prev.getParentNode() instanceof org.apache.xalan.templates.Stylesheet) )
         {
           org.apache.xalan.templates.ElemTemplateElement savedprev = prev;
 
@@ -286,20 +285,16 @@ public class Variable extends Expression implements PathComponent
               {
                 return vvar;
               }
+              vvar = null; 	 	
             }
           }
           prev = savedprev.getParentElem();
         }
       }
-
-      vvar = prev.getStylesheetRoot().getVariableOrParamComposed(m_qname);
-      if (null != vvar)
-      {
-        return vvar;
-      }
-
+      if (prev != null)
+        vvar = prev.getStylesheetRoot().getVariableOrParamComposed(m_qname);
     }
-    return null;
+    return vvar;
 
   }
   
