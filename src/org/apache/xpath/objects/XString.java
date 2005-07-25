67b6036832f53244c2050b4982728ae9f41d525e
John Gentilin <johng@apache.org>
2005-07-25 18:19:33 +0000

    PR: XALANJ-2114 Obtained from: Submitted by:	John Gentilin Reviewed by:	Henry Zongaro CVS: ----------------------------------------------------------------------
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@338224 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xpath/objects/XString.java
+++ src/org/apache/xpath/objects/XString.java
@@ -122,7 +122,7 @@ public class XString extends XObject implements XMLString
 	for (int i = 0; i < s.length(); i++)
 	{
 		char c = s.charAt(i);
-		if (c != '-' & c != '.' & ( c < 0X30 | c > 0x39)) {
+    if (c != '-' && c != '.' && ( c < 0X30 || c > 0x39)) {
             // The character is not a '-' or a '.' or a digit
             // then return NaN because something is wrong.
 			return result;
