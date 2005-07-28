0bce5bced3d541c3c8a288a6620ccdaf7c174af9
John Gentilin <johng@apache.org>
2005-07-28 00:35:59 +0000

    PR:XalanJ-2172 Obtained from: Submitted by:	John Gentilin Reviewed by:	Yash Talwar CVS: ---------------------------------------------------------------------- CVS: Change Example file to utilize the Apache Derby DBMS
    git-svn-id: https://svn.eu.apache.org/repos/asf/xalan/java/trunk@338236 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/extensions/sql/runDerby.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+#=========================================================================
+# Copyright 2004 The Apache Software Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+if [ "$DERBY_JAR_DIR" = "" ] ; then
+    DERBYJAR_DIR=.
+fi
+
+
+
+_JAVACMD=$JAVA_HOME/bin/java
+
+_BUILD_DIR=../../../build
+
+_JAR_DIR=../../../lib
+
+
+_ENDORSED_DIR=$_BUILD_DIR
+
+if [ "$JAVA_HOME" = "" ] ; then
+    echo "Warning: JAVA_HOME environment variable is not set."
+    _JAVACMD=java
+fi
+
+if [ "$PARSER_JAR" = "" ] ; then
+    PARSER_JAR=$_JAR_DIR/xercesImpl.jar
+fi
+
+if [ "$XML_APIS_JAR" = "" ] ; then
+    XML_APIS_JAR=$_JAR_DIR/xml-apis.jar
+fi
+
+if [ "$XALAN_JAR" = "" ] ; then
+    XALAN_JAR=$_BUILD_DIR/xalan.jar
+fi
+
+
+# Use _underscore prefix to not conflict with user's settings
+# Default to UNIX-style pathing
+CLPATHSEP=:
+# if we're on a Windows box make it ;
+uname | grep WIN && CLPATHSEP=\;
+
+_CLASSPATH="$XALAN_JAR${CLPATHSEP}$XML_APIS_JAR${CLPATHSEP}$PARSER_JAR${CLPATHSEP}$XALAN_JAR${CLPATHSEP}$DERBYJAR_DIR/derby.jar${CLPATHSEP}$DERBYJAR_DIR/derbytools.jar${CLPATHSEP}$CLASSPATH"
+
+# Attempt to automatically add system classes to _CLASSPATH
+if [ -f $JAVA_HOME/lib/tools.jar ] ; then
+  _CLASSPATH=${_CLASSPATH}${CLPATHSEP}${JAVA_HOME}/lib/tools.jar
+fi
+
+if [ -f $JAVA_HOME/lib/classes.zip ] ; then
+  _CLASSPATH=${_CLASSPATH}${CLPATHSEP}${JAVA_HOME}/lib/classes.zip
+fi
+
+
+echo "Running Derby: $@"
+echo "        ...with classpath: $_CLASSPATH"
+
+"$_JAVACMD" $JAVA_OPTS -Djava.endorsed.dirs=$_ENDORSED_DIR -classpath "$_CLASSPATH" -Dij.protocol=jdbc:derby: org.apache.derby.tools.ij $@
+
+
+
+
---
+++ samples/extensions/sql/runXalan.bat
@@ -0,0 +1,80 @@
+@echo off
+rem
+rem ==========================================================================
+rem = Copyright 2004 The Apache Software Foundation.
+rem =
+rem = Licensed under the Apache License, Version 2.0 (the "License");
+rem = you may not use this file except in compliance with the License.
+rem = You may obtain a copy of the License at
+rem =
+rem =     http://www.apache.org/licenses/LICENSE-2.0
+rem =
+rem = Unless required by applicable law or agreed to in writing, software
+rem = distributed under the License is distributed on an "AS IS" BASIS,
+rem = WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem = See the License for the specific language governing permissions and
+rem = limitations under the License.
+rem ==========================================================================
+rem
+rem     runDerby.bat: Script to execute the Derby tool IJ
+rem     Usage: runDerby script file] 
+rem     Setup:
+rem         - you should set JAVA_HOME
+rem         - you can set DERBY_HOME to point to both derby.jar and derbytools.jar
+rem         - JAVA_OPTS is added to the java command line
+rem         - PARSER_JAR may be set to use alternate parser (default:..\..\..\bin\xercesImpl.jar)
+rem	    - DERBY_JAR_DIR, default is the current directory
+echo.
+
+if not "%JAVA_HOME%" == "" goto setant
+:noJavaHome
+rem Default command used to call java.exe; hopefully it's on the path here
+if "%_JAVACMD%" == "" set _JAVACMD=java
+echo.
+echo Warning: JAVA_HOME environment variable is not set.
+echo   If build fails because sun.* classes could not be found
+echo   you will need to set the JAVA_HOME environment variable
+echo   to the installation directory of java.
+echo.
+
+:setant
+rem Default command used to call java.exe or equivalent
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java
+
+if "%DERBY_JAR_DIR%" == "" set DERBY_JAR_DIR=.
+
+set _DERBY_JAR=%DERBY_JAR_DIR%\derby.jar;%DERBY_JAR_DIR%\derbytools.jar
+
+set _PARSER_JAR=%PARSER_JAR%
+if "%_PARSER_JAR%" == "" set _PARSER_JAR=..\..\..\bin\xercesImpl.jar
+set _XML-APIS_JAR=%XML-APIS_JAR%
+if "%_XML-APIS_JAR%" == "" set _XML-APIS_JAR=..\..\..\bin\xml-apis.jar
+
+set _XALAN_JAR_DIR=%XALAN_JAR_DIR%
+if "%_XALAN_JAR_DIR%" == "" set _XALAN_JAR_DIR=..\..\..\build
+
+
+rem Attempt to automatically add system classes to _CLASSPATH
+rem Use _underscore prefix to not conflict with user's settings
+set _CLASSPATH=%CLASSPATH%
+if exist "%JAVA_HOME%\lib\tools.jar" set _CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%\lib\classes.zip" set _CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\classes.zip
+set _CLASSPATH=%_XALAN_JAR_DIR%\xalan.jar;%_XML-APIS_JAR%;%_PARSER_JAR%;%_DERBY_JAR%;%_CLASSPATH%
+
+echo %_CLASSPATH%
+
+@echo on
+"%_JAVACMD%" -mx64m %JAVA_OPTS% -Djava.endorsed.dirs=%_XALAN_JAR_DIR% -classpath "%_CLASSPATH%" org.apache.xalan.xslt.Process %1 %2 %3 %4 %5 %6 %7 %8
+@echo off
+
+goto end
+
+:end
+rem Cleanup environment variables
+set _JAVACMD=
+set _CLASSPATH=
+set _ANT_HOME=
+set _ANT_JAR=
+set _PARSER_JAR=
+set _XML-APIS_JAR=
+
