14b854e0e5a86bcc9ccffdbf12669cdcd5bbfb5d
Brian James Minchau <minchau@apache.org>
2006-01-12 00:26:12 +0000

    Minor, refer to Method.UNKNOWN rather than org.apache.xml.serializer.Method.UNKNOWN.
    That class and this one are in the same package, so it isn't needed.
    
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@368181 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/serializer/OutputPropertiesFactory.java
+++ src/org/apache/xml/serializer/OutputPropertiesFactory.java
@@ -298,7 +298,7 @@ public final class OutputPropertiesFactory
 
                 defaultProperties = m_text_properties;
             }
-            else if (method.equals(org.apache.xml.serializer.Method.UNKNOWN))
+            else if (method.equals(Method.UNKNOWN))
             {
                 if (null == m_unknown_properties) // double check
                 {
