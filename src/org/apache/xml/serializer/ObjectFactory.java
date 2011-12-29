b792b39257238f0432e01b3b77c56057d761b257
Michael Glavassevich <mrglavas@apache.org>
2011-12-29 16:08:00 +0000

    Minor performance improvement. Use String.lastIndexOf(int) instead of String.lastIndexOf(String).
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/trunk@1225583 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/serializer/ObjectFactory.java
+++ src/org/apache/xml/serializer/ObjectFactory.java
@@ -477,7 +477,7 @@ final class ObjectFactory {
         SecurityManager security = System.getSecurityManager();
         try{
                 if (security != null){
-                    final int lastDot = className.lastIndexOf(".");
+                    final int lastDot = className.lastIndexOf('.');
                     String packageName = className;
                     if (lastDot != -1) packageName = className.substring(0, lastDot);
                     security.checkPackageAccess(packageName);
