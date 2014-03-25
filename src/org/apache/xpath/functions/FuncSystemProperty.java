52c27d504ced40719d243a8715d7f67d531723da
Gary D. Gregory <ggregory@apache.org>
2014-03-25 17:47:08 +0000

    Adjustment to patch for secure processing for CVE-2014-0107 by Ryan Berg based on feedack from Emmanuel Bourg.
    git-svn-id: https://svn.apache.org/repos/asf/xalan/java/branches/xalan-j_2_7_1_maint@1581426 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xpath/functions/FuncSystemProperty.java
+++ src/org/apache/xpath/functions/FuncSystemProperty.java
@@ -100,13 +100,12 @@ public class FuncSystemProperty extends FunctionOneArg
             //if secure procession is enabled only handle required properties do not not map any valid system property
             if(!xctxt.isSecureProcessing())
             {
-                result = System.getProperty(fullName);
+                result = System.getProperty(propName);
             }
             else
             {
                 warn(xctxt, XPATHErrorResources.WG_SECURITY_EXCEPTION,
                         new Object[]{ fullName });  //"SecurityException when trying to access XSL system property: "+fullName);
-                result = xsltInfo.getProperty(propName);
             }
             if (null == result)
             {
@@ -135,7 +134,6 @@ public class FuncSystemProperty extends FunctionOneArg
           {
               warn(xctxt, XPATHErrorResources.WG_SECURITY_EXCEPTION,
                       new Object[]{ fullName });  //"SecurityException when trying to access XSL system property: "+fullName);
-              result = xsltInfo.getProperty(propName);
           }
           if (null == result)
           {
